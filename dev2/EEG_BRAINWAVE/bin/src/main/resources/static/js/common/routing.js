b5a4e4bade8107d229d35f6ffc7cc44f6ed23ff9
김선형 <lobsterbuger@naver.com>
2025-09-01 10:20:15 +0900

    머지 성공

--- dev2/EEG_BRAINWAVE/bin/src/main/resources/static/js/common/routing.js
+++
@@ -1,9 +0,0 @@
-function goToMain(){
-	window.location.href = "/mainPage";
-} 
-function goToLogin(){
-	window.location.href = "/loginPage";
-} 
-function goToJoin(){
-	window.location.href = "/joinPage";
-} 
